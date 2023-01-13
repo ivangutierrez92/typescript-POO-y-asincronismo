c5889007e46a9f1e8d2920d47b4674cd7d3852f7
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2023-01-13 19:52:52 -0300

    private data

---
+++ src/04-private.ts
@@ -0,0 +1,42 @@
+export class MyDate {
+  year: number;
+  month: number;
+  private day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    return `${this.addPadding(this.day)}/${this.addPadding(this.month)}/${
+      this.year
+    }`;
+  }
+
+  private addPadding(value: number) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return `${value}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+  getDay() {
+    return this.day;
+  }
+}
+
+const myDate = new MyDate(1993, 7, 9);
+console.log(myDate.printFormat());
