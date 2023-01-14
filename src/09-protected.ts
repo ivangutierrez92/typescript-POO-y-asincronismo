fd93cdb45574c612caa22187e1a466bcda041701
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2023-01-14 13:54:27 -0300

    protected

---
+++ src/09-protected.ts
@@ -0,0 +1,32 @@
+export class Animal {
+  constructor(protected name: string) {}
+
+  move() {
+    console.log('Moving!');
+  }
+
+  greeting() {
+    return `Hello, I'm ${this.name}`;
+  }
+}
+
+export class Dog extends Animal {
+  constructor(name: string, public owner: string) {
+    super(name);
+  }
+
+  woof(times: number) {
+    for (let index = 0; index < times; index++) {
+      console.log(`woof! ${this.name}`);
+    }
+  }
+
+  move() {
+    console.log('moving as a dog');
+    super.move();
+  }
+}
+
+const cheis = new Dog('cheis', 'nico');
+cheis.woof(1);
+cheis.move();
