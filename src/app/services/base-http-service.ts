e34eef6f6c974cf2fcb0ea3112eea1f11d594786
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2023-01-15 16:57:07 -0300

    genericos en métodos

--- src/app/services/base-http-service.ts
+++ src/app/services/base-http-service.ts
@@ -1,14 +1,20 @@
 import { Category } from '../models/category.model';
 import axios from 'axios';
 import { Product } from '../models/product.model';
+import { UpdateProductDto } from '../dtos/product.dto';
 
 export class BaseHttpService<t> {
   //data: t[] = [];
 
-  constructor(private url: string) {}
+  constructor(protected url: string) {}
 
   async getAll() {
     const { data } = await axios.get<t[]>(this.url);
+    return data;
+  }
+
+  async update<ID, DTO>(id: ID, changes: DTO) {
+    const { data } = await axios.put(`${this.url}/${id}`, changes);
     return data;
   }
 }
@@ -18,7 +24,10 @@ export class BaseHttpService<t> {
     'https://api.escuelajs.co/api/v1/products'
   );
   const rta = await productService.getAll();
-  console.log(rta);
+  console.log('products', rta.length);
+  productService.update<Product['id'], UpdateProductDto>(1, {
+    title: 'new title',
+  });
 
   const categoryService = new BaseHttpService<Category>(
     'https://api.escuelajs.co/api/v1/categories'
