5514f9b92866561220d4e222c3d3562fb7a19a10
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2023-01-15 11:28:43 -0300

    funciones a clase

---
+++ src/app/dtos/product.dto.ts
@@ -0,0 +1,11 @@
+import { Category } from "../models/category.model";
+import { Product } from "../models/product.model";
+
+export interface CreateProductDto
+  extends Omit<Product, 'id' | 'creationAt' | 'updatedAt' | 'category'> {
+  categoryId: Category['id'];
+}
+
+export interface UpdateProductDto extends Partial<CreateProductDto> {}
+
+
---
+++ src/app/services/product-memory.service.ts
@@ -0,0 +1,40 @@
+import { Product } from '../models/product.model';
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+import { faker } from '@faker-js/faker';
+
+export class productMemoryService {
+  private products: Product[] = [];
+
+  create = (data: CreateProductDto): Product => {
+    const newProduct = {
+      ...data,
+      id: faker.datatype.number(),
+      creationAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+      category: {
+        id: data.categoryId,
+        name: faker.commerce.department(),
+        image: faker.image.imageUrl(),
+        creationAt: faker.date.recent(),
+        updatedAt: faker.date.recent(),
+      },
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  };
+
+  add(product: Product) {
+    this.products.push(product);
+    return product;
+  }
+  updateProduct = (id: Product['id'], changes: UpdateProductDto): Product => {
+    const index = this.products.findIndex((item) => item.id === id);
+    const prevData = this.products[index];
+    this.products[index] = { ...prevData, ...changes };
+    return this.products[index];
+  };
+
+  findOne(id: Product['id']) {
+    return this.products.find((item) => item.id === id);
+  }
+}
